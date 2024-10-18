1f5fd97010feab59d31ed29c51cf3b35335c1db1
salmaKhaled-dot <sokhalled0@gmail.com>
2024-10-18 16:31:56 +0300

    Add files via upload

---
+++ pagethree/script.js
@@ -0,0 +1,235 @@
+﻿function hidePreloader() {
+    setTimeout(function () {
+        document.querySelector(".preloader").style.display = "none";
+    }, 2000);
+}
+
+window.addEventListener("load", hidePreloader);
+
+$(document).ready(function () {
+    $("#search").focus(function () {
+        $(".search-box").addClass("border-searching");
+        $(".search-icon").addClass("si-rotate");
+    });
+    $("#search").blur(function () {
+        $(".search-box").removeClass("border-searching");
+        $(".search-icon").removeClass("si-rotate");
+    });
+    $("#search").keyup(function () {
+        if ($(this).val().length > 0) {
+            $(".go-icon").addClass("go-in");
+        } else {
+            $(".go-icon").removeClass("go-in");
+        }
+    });
+    $(".go-icon").click(function () {
+        $(".search-form").submit();
+    });
+});
+
+$(document).ready(function () {
+    var body = $("body");
+    var menu = $(".menu-icon");
+    var menuItems = $(".nav__list-item");
+
+    menu.on("click", function () {
+        body.toggleClass("nav-active");
+    });
+});
+var video = document.getElementById("overlayVideo");
+var btn = document.getElementById("videoControlBtn");
+
+function toggleVideo() {
+    if (video.paused) {
+        video.play();
+        btn.classList.add("playing");
+        btn.setAttribute("aria-label", "Pause Video");
+    } else {
+        video.pause();
+        btn.classList.remove("playing");
+        btn.setAttribute("aria-label", "Play Video");
+    }
+
+    var icon = btn.classList.contains("playing")
+        ? "pauseVideo.svg"
+        : "playVideo.svg";
+    btn.style.backgroundImage = `url(${icon})`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tabPanes = document.getElementsByClassName("tab-pane");
+    for (var i = 0; i < tabPanes.length; i++) {
+        tabPanes[i].style.display = "none";
+    }
+});
+
+function changeTab(index) {
+    var buttonBoxes = document.getElementsByClassName("button-box");
+    var tabPanes = document.getElementsByClassName("tab-pane");
+
+    for (var i = 0; i < buttonBoxes.length; i++) {
+        buttonBoxes[i].classList.remove("active");
+    }
+
+    for (var j = 0; j < tabPanes.length; j++) {
+        tabPanes[j].style.display = "none";
+    }
+
+    buttonBoxes[index].classList.add("active");
+    tabPanes[index].style.display = "block";
+}
+
+(function ($) {
+    "use strict";
+    s();
+    for (
+        var r = document.querySelectorAll(".hover-target"), a = r.length - 1;
+        a >= 0;
+        a--
+    ) {
+        o(r[a]);
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s);
+    }
+
+    var app = (function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector("body");
+            menu = document.querySelector(".menu-icon");
+            menuItems = document.querySelectorAll(".nav__list-item");
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener("click", function () {
+                return toggleClass(body, "nav-active");
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass))
+                element.classList.remove(stringClass);
+            else element.classList.add(stringClass);
+        };
+        init();
+    })();
+})(jQuery);
+$(document).ready(function () {
+    var body = $("body");
+    var menu = $(".menu-icon");
+    var menuItems = $(".nav__list-item");
+
+    menu.on("click", function () {
+        body.toggleClass("nav-active");
+        menu.toggleClass("close-icon");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tabPanes = document.getElementsByClassName("tab-pane");
+    for (var i = 0; i < tabPanes.length; i++) {
+        tabPanes[i].style.display = "none";
+    }
+});
+
+function changeTab(index) {
+    var buttonBoxes = document.getElementsByClassName("button-box");
+    var tabPanes = document.getElementsByClassName("tab-pane");
+
+    for (var i = 0; i < buttonBoxes.length; i++) {
+        buttonBoxes[i].classList.remove("active");
+    }
+
+    for (var j = 0; j < tabPanes.length; j++) {
+        tabPanes[j].style.display = "none";
+    }
+
+    buttonBoxes[index].classList.add("active");
+    tabPanes[index].style.display = "block";
+}
+
+function getDoctorAvailability() {
+    var specialty = document.getElementById("specialty").value;
+
+    var doctors = [
+        {
+            name: "Adel Gamel",
+            specialty: "cardiology",
+            availability: "10:00 AM - 12:00 PM",
+        },
+        {
+            name: "Mohamed Khaled",
+            specialty: "dermatology",
+            availability: "2:00 PM - 4:00 PM",
+        },
+        {
+            name: "Ashraf Essam",
+            specialty: "cardiology",
+            availability: "9:00 AM - 11:00 AM",
+        },
+        {
+            name: "Noha Ahmed",
+            specialty: "endocrinology",
+            availability: "1:00 PM - 3:00 PM",
+        },
+        {
+            name: "Habiba Loai",
+            specialty: "dermatology",
+            availability: "10:00 AM - 12:00 PM",
+        },
+        {
+            name: "Mahmoud Moustafa",
+            specialty: "cardiology",
+            availability: "2:00 PM - 4:00 PM",
+        },
+    ];
+
+    var doctorNamesBody = document.getElementById("doctorNamesBody");
+    var availabilityBody = document.getElementById("availabilityBody");
+    var tablesContainer = document.getElementById("tablesContainer");
+
+    doctorNamesBody.innerHTML = "";
+    availabilityBody.innerHTML = "";
+
+    var filteredDoctors = doctors.filter(function (doctor) {
+        return doctor.specialty === specialty;
+    });
+
+    for (var i = 0; i < filteredDoctors.length; i++) {
+        var doctor = filteredDoctors[i];
+
+        var nameRow = document.createElement("tr");
+        var nameCell = document.createElement("td");
+        nameCell.innerText = doctor.name;
+        nameRow.appendChild(nameCell);
+        doctorNamesBody.appendChild(nameRow);
+
+        var availabilityRow = document.createElement("tr");
+        var availabilityCell = document.createElement("td");
+        availabilityCell.innerText = doctor.availability;
+        availabilityRow.appendChild(availabilityCell);
+        availabilityBody.appendChild(availabilityRow);
+    }
+
+    if (filteredDoctors.length > 0) {
+        tablesContainer.style.display = "block";
+    } else {
+        tablesContainer.style.display = "none";
+    }
+}
+
+const yesRadio = document.getElementById("yes");
+const noRadio = document.getElementById("no");
+
+yesRadio.addEventListener("change", handleSmartwatchSelection);
+noRadio.addEventListener("change", handleSmartwatchSelection);
+
+function handleSmartwatchSelection() {
+    if (yesRadio.checked) {
+        console.log("User has a smartwatch");
+    } else if (noRadio.checked) {
+        console.log("User does not have a smartwatch");
+    }
+}
---
+++ pagethree/transl.js
@@ -0,0 +1,228 @@
+﻿const locales = ["en-GB", "ar-EG"];
+
+function getFlagSrc(countryCode) {
+    return /^[A-Z]{2}$/.test(countryCode)
+        ? `https://www.countryflagicons.com/SHINY/64/${countryCode.toUpperCase()}.png`
+        : "";
+}
+
+function translatePage(language) {
+    const isArabic = language === "ar-EG";
+
+    const translations = {
+        "en-GB": {
+            "tagline-text": "We care about you",
+            "home-link": "Home",
+            "findDoctor-link": "Find a doctor",
+            "aiDiagnose-link": "AI Diagnose",
+            "warning-link": "Warning",
+            "nearYou-link": "Near you",
+            "tabOne-text": "Find a Doctor",
+            "tabTwo-text": "Near Hospitals and Pharmacy",
+            "tabThree-text": "Appointment",
+            "tabFour-text": "Warning",
+            "medicalSpecialty-text": "Medical specialty?",
+            "onlineOrNearYou-text": "Online or near you",
+            "online-text": "Online",
+            "nearYou-text": "Near You",
+            "showResult-button": "Show the result",
+            "clinicOrPharmacy-text": "Clinic or Pharmacy",
+            "clinic-option": "Clinic",
+            "pharmacy-option": "Pharmacy",
+            "specialty-text": "What specialty are you looking for?",
+            "cardiology-option": "Cardiology",
+            "dermatology-option": "Dermatology",
+            "endocrinology-option": "Endocrinology",
+            "gastroenterology-option": "Gastroenterology",
+            "neurology-option": "Neurology",
+            "oncology-option": "Oncology",
+            "pediatrics-option": "Pediatrics",
+            "psychiatry-option": "Psychiatry",
+            "radiology-option": "Radiology",
+            "surgery-option": "Surgery",
+            "kindAppointment-text":
+                "What kind of appointment are you scheduling?",
+            "physical-option": "Physical",
+            "virtual-option": "Virtual",
+            "chooseDoctor-heading": "Choose Doctor",
+            "calendar-heading": "Calendar",
+            "bookAppointment-button": "Book an appointment",
+            "smartwatchQuestion-text": "Do you have a smartwatch?",
+            "yes-label": "Yes",
+            "no-label": "No",
+            "warnSign-text": "You can send warn sign if you are in danger",
+            "warning-button": "Warning",
+            "downloadApp-button":
+                "Download our app and connect your smartwatch",
+            "lifesaver-text1": "Be a Lifesaver",
+            "lifesaver-text2": " Donate Blood and Make a Difference Today!",
+            "donateCheckup-text": "Donate and take a free checkup :)",
+            "news-heading": "News",
+            "followUs-heading": "Follow Us",
+            "news-section": "News",
+            "aboutUs-section": "About Us",
+            "tabOne-heading": "Looking For",
+            "tabOne-heading": "Looking For",
+            finddoctor: "FIND A DOCTOR",
+            aidiagnose: "AI DIAGNOSE",
+            Warning: "WARNING",
+            nearyou: "NEAR YOU",
+            "tabOne-heading2": "Find Doctors and Dentists Near You",
+            search: "Search doctores",
+            "search-paragraph":
+                "You can also search by physician, practice, or hospital name",
+            "bu-text": "Book an appointment",
+        },
+        "ar-EG": {
+            "tagline-text": "نحن نهتم بك",
+            "home-link": "الرئيسية",
+            "findDoctor-link": "البحث عن طبيب",
+            "aiDiagnose-link": "التشخيص بالذكاء الاصطناعي",
+            "warning-link": "الطوارئ",
+            "nearYou-link": "بالقرب منك",
+            "tabOne-text": "البحث عن طبيب",
+            "tabTwo-text": "المستشفيات والصيدليات بالقرب منك",
+            "tabThree-text": "حجز موعد",
+            "tabFour-text": "الطوارئ",
+            "medicalSpecialty-text": "تخصص طبي؟",
+            "onlineOrNearYou-text": "عبر الإنترنت أو قريب منك",
+            "online-text": "عبر الإنترنت",
+            "nearYou-text": "قريب منك",
+            "showResult-button": "عرض النتيجة",
+            "clinicOrPharmacy-text": "عيادة أو صيدلية",
+            "clinic-option": "عيادة",
+            "pharmacy-option": "صيدلية",
+            "specialty-text": "ما هو التخصص الذي تبحث عنه؟",
+            "cardiology-option": "القلبية",
+            "dermatology-option": "الجلدية",
+            "endocrinology-option": "الغدد الصماء",
+            "gastroenterology-option": "الجهاز الهضمي",
+            "neurology-option": "الأعصاب",
+            "oncology-option": "الأورام",
+            "pediatrics-option": "طب الأطفال",
+            "psychiatry-option": "الطب النفسي",
+            "radiology-option": "الأشعة",
+            "surgery-option": "الجراحة",
+            "kindAppointment-text": "ما نوع الموعد الذي تريد حجزه؟",
+            "physical-option": "حضوري",
+            "virtual-option": "افتراضي",
+            "chooseDoctor-heading": "اختيار الطبيب",
+            "calendar-heading": "التقويم",
+            "bookAppointment-button": "حجز موعد",
+            "smartwatchQuestion-text": "هل لديك ساعة ذكية؟",
+            "yes-label": "نعم",
+            "no-label": "لا",
+            "warnSign-text": "يمكنك إرسال إشارة تحذير إذا كنت في خطر",
+            "warning-button": "تحذير",
+            "downloadApp-button":
+                "قم بتحميل التطبيق وقم بتوصيل الساعة الذكية الخاصة بك",
+            "lifesaver-text1": "كن منقذًا",
+            "lifesaver-text2": "تبرع بالدم واصنع فرقًا اليوم !",
+            "donateCheckup-text": "تبرع واحصل على فحص مجاني :)",
+            "news-heading": "أخبار",
+            "followUs-heading": "تابعنا",
+            "news-section": "أخبار",
+            "aboutUs-section": "من نحن",
+            "tabOne-heading": "بحث عن",
+            "tabTwo-heading": "بحث عن",
+            finddoctor: "البحث عن طبيب",
+            aidiagnose: "التشخيص بالذكاء الاصطناعي",
+            Warning: "الطوارئ",
+            nearyou: "بالقرب منك",
+            "tabOne-heading2": "البحث عن أطباء بالقرب منك",
+            search: "البحث عن الاطباء",
+            "search-paragraph":
+                "يمكنك أيضًا البحث باسم الطبيب أو الممارسة أو المستشفى",
+            "bu-text": "احجز الان",
+        },
+    };
+
+    for (const key in translations[language]) {
+        const element = document.getElementById(key);
+        if (element) {
+            element.textContent = translations[language][key];
+            if (isArabic) {
+                element.classList.add("arabic-style");
+            } else {
+                element.classList.remove("arabic-style");
+            }
+        }
+    }
+
+    const buttonsElement = document.querySelector(".buttons");
+    if (isArabic) {
+        buttonsElement.classList.add("arabic-buttons");
+    } else {
+        buttonsElement.classList.remove("arabic-buttons");
+    }
+    function updateImageWrapperFlexDirection() {
+        const boxDonate = document.querySelector(".box-donate");
+        const imageWrapper = boxDonate.querySelector(".image-wrapper");
+        if (isArabic) {
+            imageWrapper.style.flexDirection = "row";
+        } else {
+            imageWrapper.style.flexDirection = "row-reverse";
+        }
+    }
+
+    updateImageWrapperFlexDirection();
+
+    document.addEventListener("DOMContentLoaded", function () {
+        document.addEventListener(
+            "langchange",
+            updateImageWrapperFlexDirection
+        );
+    });
+}
+
+function setSelectedLocale(locale) {
+    const intlLocale = new Intl.Locale(locale);
+    const langName = new Intl.DisplayNames([locale], {
+        type: "language",
+    }).of(intlLocale.language);
+
+    const dropdownBtn = document.getElementById("dropdown-btn");
+    dropdownBtn.innerHTML = `<img src="${getFlagSrc(
+        intlLocale.region
+    )}" /> ${langName}`;
+
+    translatePage(locale);
+}
+
+function handleLanguageSelection() {
+    const dropdownBtn = document.getElementById("dropdown-btn");
+    const dropdownContent = document.getElementById("dropdown-content");
+    let isDropdownOpen = false;
+
+    dropdownBtn.addEventListener("click", function () {
+        if (isDropdownOpen) {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+        isDropdownOpen = !isDropdownOpen;
+    });
+
+    locales.forEach((locale) => {
+        const intlLocale = new Intl.Locale(locale);
+        const langName = new Intl.DisplayNames([locale], {
+            type: "language",
+        }).of(intlLocale.language);
+
+        const listEl = document.createElement("li");
+        listEl.innerHTML = `<img src="${getFlagSrc(
+            intlLocale.region
+        )}" /> ${langName}`;
+
+        listEl.addEventListener("click", function () {
+            setSelectedLocale(locale);
+            dropdownContent.style.display = "none";
+            isDropdownOpen = false;
+        });
+
+        dropdownContent.appendChild(listEl);
+    });
+}
+
+handleLanguageSelection();
+setSelectedLocale(locales[0]);
